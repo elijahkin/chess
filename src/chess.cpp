2f78b46f92c362ec856bb1c12bd0a25d5ebd26ae
Elijah Kin <elijah.m.kin@gmail.com>
2024-06-26 23:13:03 -0700

    Move-making, alternation, and better printing

--- src/chess.cpp
+++ src/chess.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+// #include <format>
 
 // Define color scheme via ANSI escape codes
 const std::string kWhiteText = "\33[37m";
@@ -11,11 +12,6 @@ const std::string kBlackBackground = "\33[48;5;20m";
 const std::string kResetBackground = "\33[49m";
 
 typedef int8_t Square;
-
-struct Move {
-  Square from;
-  Square to;
-};
 
 enum Color : int8_t { kWhite, kBlack };
 
@@ -62,6 +58,7 @@ class Board {
 private:
   std::vector<int8_t> squares_;
   std::vector<Piece> pieces_;
+  Color colorToMove_;
 
 public:
   // Set up the board with the pieces in their starting positions
@@ -72,40 +69,36 @@ public:
     }
 
     pieces_.reserve(32);
-    for (int8_t i = 0; i < 8; i++) {
+    for (int8_t i = 0; i < 8; ++i) {
       CreatePiece(kBlack, major_rank[i], i);
       CreatePiece(kBlack, kPawn, i + 8);
       CreatePiece(kWhite, kPawn, i + 48);
       CreatePiece(kWhite, major_rank[i], i + 56);
     }
+
+    colorToMove_ = kWhite;
   }
 
   friend std::ostream &operator<<(std::ostream &stream, const Board &board) {
-    bool isWhiteSquare = true;
+    stream << '\n';
     for (int8_t rank = 0; rank < 8; ++rank) {
-      stream << static_cast<int>(8 - rank);
-      stream << " ";
+      stream << static_cast<int>(8 - rank) << ' ';
       for (int8_t file = 0; file < 8; ++file) {
-        stream << (isWhiteSquare ? kWhiteBackground : kBlackBackground);
+        stream << ((rank + file) % 2 ? kBlackBackground : kWhiteBackground);
         int8_t square = board.squares_[rank * 8 + file];
-        stream << " ";
+        stream << ' ';
         if (square == -1) {
-          stream << " ";
+          stream << ' ';
         } else {
           stream << board.pieces_[square];
         }
-        stream << " ";
-        isWhiteSquare = !isWhiteSquare;
+        stream << ' ';
       }
-      stream << kResetBackground;
-      stream << "\n";
-      isWhiteSquare = !isWhiteSquare;
+      stream << kResetBackground << '\n';
     }
     stream << "  ";
-    for (int8_t file = 0; file < 8; ++file) {
-      stream << " ";
-      stream << static_cast<char>('a' + file);
-      stream << " ";
+    for (char file = 'a'; file <= 'h'; ++file) {
+      stream << ' ' << file << ' ';
     }
     return stream;
   }
@@ -114,6 +107,11 @@ public:
     squares_[square] = pieces_.size();
     pieces_.push_back({color, type, square});
   }
+
+  struct Move {
+    Square from;
+    Square to;
+  };
 
   // https://en.wikipedia.org/wiki/Algebraic_notation_(chess)
   // K for king, Q for queen, R for rook, B for bishop, and N for knight
@@ -143,18 +141,17 @@ public:
       break;
     }
 
+    std::vector<Square> candidates;
     for (auto piece : pieces_) {
-      if (piece.type == piece_type) {
-        std::cout << "Found piece of correct type at square "
-                  << static_cast<int>(piece.square) << std::endl;
-        // Is it viable?
+      if (piece.type == piece_type && piece.color == colorToMove_) {
+        // TODO Is it viable?
+        candidates.push_back(piece.square);
       }
     }
 
-    const int8_t from = 0; // TODO
-
-    // Use file and rank from algebraic notation to compute the to index
-    const int8_t to = (move[1] - 'a') * 8 + (move[2] - '0');
+    assert(candidates.size() == 1);
+    const int8_t from = candidates[0];
+    const int8_t to = (move[1] - 'a') + 8 * ('8' - move[2]);
 
     return {from, to};
   }
@@ -163,7 +160,8 @@ public:
   // TODO std::vector<Square> getFile() {}
   // TODO std::vector<Square> getDiagonal() {}
   // TODO std::vector<Square> getAntiDiagonal() {}
-  // TODO bool isOccupied(Square square) {}
+
+  inline bool isOccupied(Square square) { return (squares_[square] == -1); }
 
   std::vector<Square> LegalMovesForPiece(Piece piece) {
     std::vector<Square> moves;
@@ -174,7 +172,8 @@ public:
       break;
     case kRook:
       for (int i = 0; i < 8; ++i) {
-        moves.push_back(8 * (piece.square / 8) + i); // file
+        Square square = 8 * (piece.square / 8) + i;
+        moves.push_back(square); // file
         // if occupied, break
       }
       for (int i = 0; i < 8; ++i) {
@@ -195,11 +194,45 @@ public:
     }
     return moves;
   }
+
+  // TODO should verify that the move is legal
+  inline void MakeMove(Square from, Square to) {
+    // Update the square field of the captured and moving piece...
+    // pieces_[squares_[to]].square = -1;
+    pieces_[squares_[from]].square = to;
+    // ...and update the indices stored in squares_
+    squares_[to] = squares_[from];
+    squares_[from] = -1;
+    // Finally, switch to the other player
+    colorToMove_ = ((colorToMove_ == kWhite) ? kBlack : kWhite);
+  }
 };
 
 int main() {
   Board board;
-  std::cout << board << std::endl;
-  // board.ParseAlgebraicNotation("Be5");
+  std::vector<std::string> history;
+  std::string input;
+  while (true) {
+    // Clear the screen
+    // TODO possibly move this into print
+    system("clear");
+
+    // Print out the board and the history
+    // TODO move this into board print
+    std::cout << board << "\n" << std::endl;
+    for (int8_t move_number = 0; move_number < history.size(); ++move_number) {
+      std::cout << (move_number + 1) << ". " << history[move_number] << " ";
+    }
+    std::cout << "\n" << std::endl;
+
+    // Get use input for the move
+    // TODO overload >> instead of having a function
+    std::cout << "Please enter a move: ";
+    std::cin >> input;
+    history.push_back(input);
+    Board::Move move = board.ParseAlgebraicNotation(input);
+
+    board.MakeMove(move.from, move.to);
+  }
   return 0;
 }
