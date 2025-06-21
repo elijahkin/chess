e2f12a83822f3565b22e3ed1dd40e55c68f6aac7
Elijah Kin <elijah.m.kin@gmail.com>
2025-06-21 15:20:51 -0700

    Separating minimax logic into its own class

---
+++ src/agents/minimax_agent.h
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+#include "../base.h"
+
+static const double kInf = std::numeric_limits<double>::infinity();
+static const double kNegInf = -std::numeric_limits<double>::infinity();
+
+// Implements the minimax algorithm for abstract two-player adversarial games.
+// TODO Make this inherit from an abstract `Agent` class?
+template <typename Move>
+class MinimaxAgent {
+ public:
+  explicit MinimaxAgent(int max_plies) : max_plies_(max_plies) {}
+
+  // The shallowest level of the minimax search is separate because we want to
+  // return the move itself instead of its value
+  // TODO Consider using iterative deepening instead
+  Move select_move(Game<Move> &state) {
+    double best_value = kNegInf;
+    std::vector<Move> best_moves;
+
+    for (const auto &move : state.GetMoves()) {
+      const double value = alphabeta(state, move, 1, kNegInf, kInf);
+      if (value > best_value) {
+        best_moves.clear();
+        best_moves.push_back(move);
+        best_value = value;
+      } else if (value == best_value) {
+        best_moves.push_back(move);
+      }
+    }
+    // TODO If there are multiple optimal moves, we may want to randomly select
+    // among them
+    return best_moves[0];
+  }
+
+ private:
+  const int max_plies_;
+
+  // A helper function for performing the minimax algorithm with alpha-beta
+  // pruning, taking heavy inspiration from
+  // https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning#Pseudocode
+  double alphabeta(Game<Move> &state, const Move &move, int ply, double alpha,
+                   double beta) {
+    state.MakeMove(move);
+
+    if (ply == max_plies_) {
+      // TODO Consider using transposition tables
+      double value = state.Valuate();
+      state.RevertMove(move);
+      return value;
+    }
+
+    double value;
+    if (ply % 2 == 0) {  // Maximizing player
+      value = kNegInf;
+      for (const auto &move : state.GetMoves()) {
+        value = std::max(value, alphabeta(state, move, ply + 1, alpha, beta));
+        if (value >= beta) {
+          break;
+        }
+        alpha = std::max(alpha, value);
+      }
+    } else {  // Minimizing player
+      value = kInf;
+      for (const auto &move : state.GetMoves()) {
+        value = std::min(value, alphabeta(state, move, ply + 1, alpha, beta));
+        if (value <= alpha) {
+          break;
+        }
+        beta = std::min(beta, value);
+      }
+    }
+    state.RevertMove(move);
+    return value;
+  }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+#include "agents/minimax_agent.h"
 #include "games/chess.h"
 
 const std::string kClearTerminal = "\033[2J";
@@ -9,30 +10,32 @@ const std::string kCursorTopLeft = "\033[1;1H";
 int main() {
   Chess game = Chess(/*white_perspective=*/true);
 
+  auto minimax = MinimaxAgent<ChessMove>(5);
+
   while (true) {
     // Human player
-    std::cout << kClearTerminal << kCursorTopLeft;
-    std::cout << game.ToString() << "\n";
+    std::cout << kClearTerminal << kCursorTopLeft << game.ToString() << "\n";
 
     // Loop until the user enters a valid move
     std::string input;
+    ChessMove white_move;
     while (true) {
       std::cout << "Please enter a move: ";
       std::cin >> input;
       auto parsed = game.ParseAlgebraicNotation(input);
       if (parsed.has_value()) {
-        game.MakeMakeWithHistory(parsed.value());
+        white_move = parsed.value();
         break;
       }
       std::cout << "Invalid entry! ";
     }
+    game.MakeMakeWithHistory(white_move);
 
     // Minimax agent player
-    std::cout << kClearTerminal << kCursorTopLeft;
-    std::cout << game.ToString() << "\n";
+    std::cout << kClearTerminal << kCursorTopLeft << game.ToString() << "\n";
 
     std::cout << "Minimax agent is thinking...\n";
-    ChessMove black_move = game.Minimax(5);
+    ChessMove black_move = minimax.select_move(game);
     game.MakeMakeWithHistory(black_move);
   }
   return 0;
