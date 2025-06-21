bde697efa33c25c7d780ce243e7e80c64f16579d
Elijah Kin <elijah.m.kin@gmail.com>
2025-06-21 11:20:38 -0700

    Perestroika

--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 CC = g++
-CFLAGS = -std=c++23 -O2 -Wall -Wextra -Wpedantic
+CFLAGS = -std=c++23 -O2 -Wall -Wextra -Wpedantic -Werror
 
 all: chess
 
-chess: src/chess.cpp
-	$(CC) $(CFLAGS) -o bin/chess src/chess.cpp
+chess: src/main.cpp
+	$(CC) $(CFLAGS) -o bin/chess src/main.cpp
 
 clean:
 	rm -f bin/*
--- src/games/chess.h
+++ src/games/chess.h
@@ -3,8 +3,8 @@
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
-#include <iostream>
 #include <limits>
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -407,32 +407,3 @@ class Chess {
     MakeMove(move);
   }
 };
-
-int main() {
-  Chess game = Chess(true);
-  std::string input;
-  while (true) {
-    // Clear the screen and print out the board with history
-    system("clear");
-    std::cout << game << "\n";
-
-    // for (auto move : game.LegalMoves()) {
-    //   std::cout << game.ToString(move) << ' ';
-    // }
-    // std::cout << std::endl;
-
-    // Allow the user to input a move
-    std::cout << "Please enter a move: ";
-    // TODO Overload >> instead of having a function?
-    std::cin >> input;
-    Chess::Move white_move = game.ParseAlgebraicNotation(input);
-    game.MakeMakeWithHistory(white_move);
-
-    system("clear");
-    std::cout << game << "\n";
-
-    Chess::Move black_move = game.Minimax(5);
-    game.MakeMakeWithHistory(black_move);
-  }
-  return 0;
-}
